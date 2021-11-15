e97e550653c0c323a5de7b43570bbb5521cc564c
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-11-15 16:40:13 +0700

    Navigating Programmatically

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,33 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Home: NextPage = () => (
-  <>
-    <h1>Home</h1>
-    <Link href="/about">
-      <button type="button">about</button>
-    </Link>
-    <Link href="/user">
-      <button type="button">user</button>
-    </Link>
-  </>
-);
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    console.log('Placing Order');
+    router.push('/product');
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/about">
+        <button type="button">about</button>
+      </Link>
+      <Link href="/user">
+        <button type="button">user</button>
+      </Link>
+      <br />
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Place Order
+      </button>
+    </>
+  );
+};
 
 export default Home;
