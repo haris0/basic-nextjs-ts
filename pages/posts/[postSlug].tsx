b914a25649a35553e99eba78de5ce2e1e91c93ff
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-11-19 10:44:49 +0700

    getStaticPaths fallback true

--- pages/posts/[postSlug].tsx
+++ pages/posts/[postSlug].tsx
@@ -4,27 +4,28 @@ import type {
   InferGetStaticPropsType,
   NextPage,
 } from 'next';
+import { useRouter } from 'next/router';
 import { PostType } from '../../types';
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data: PostType[] = await response.json();
-  const paths = data.map((post) => ({
-    params: {
-      postSlug: post.id.toString(),
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [
+    {
+      params: { postSlug: '1' },
     },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
+  ],
+  fallback: true,
+});
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const { params } = context;
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params?.postSlug}`);
   const data: PostType = await response.json();
+
+  if (!data.id) {
+    return { notFound: true };
+  }
+
+  console.log(`Generate page for /post/${params?.postSlug}`);
 
   return {
     props: {
@@ -33,11 +34,20 @@ export const getStaticProps: GetStaticProps = async (context) => {
   };
 };
 
-const PostDetail: NextPage = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => (
-  <>
-    <h2>{ `${post.id}. ${post.title}` }</h2>
-    <p>{ post.body }</p>
-  </>
-);
+const PostDetail: NextPage = ({ post }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+
+  return (
+    <div className="detail-post">
+      {router.isFallback && <h1>Loading...</h1>}
+      {!router.isFallback && (
+        <>
+          <h2>{`${post.id}. ${post.title}`}</h2>
+          <p>{post.body}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default PostDetail;
