c05c440e2320cfc72ac8a02ca22aa81bae664781
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-11-26 14:06:54 +0700

    implement Component Level Styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -11,26 +12,23 @@ const Home: NextPage = () => {
   };
 
   return (
-    <>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Home</h1>
       <Link href="/about" passHref>
         <button type="button" className="btn btn-primary">
           about
         </button>
       </Link>
-      <br />
       <Link href="/users" passHref>
         <button type="button" className="btn btn-primary">
           user
         </button>
       </Link>
-      <br />
       <Link href="/posts" passHref>
         <button type="button" className="btn btn-primary">
           Post
         </button>
       </Link>
-      <br />
       <button
         type="button"
         className="btn btn-primary"
@@ -38,31 +36,27 @@ const Home: NextPage = () => {
       >
         Product
       </button>
-      <br />
       <Link href="/news" passHref>
         <button type="button" className="btn btn-primary">
           News
         </button>
       </Link>
-      <br />
       <Link href="/dashboard" passHref>
         <button type="button" className="btn btn-primary">
           Dashboard
         </button>
       </Link>
-      <br />
       <Link href="/events" passHref>
         <button type="button" className="btn btn-primary">
           Events
         </button>
       </Link>
-      <br />
       <Link href="/comments" passHref>
         <button type="button" className="btn btn-primary">
           Comments
         </button>
       </Link>
-    </>
+    </div>
   );
 };
 
