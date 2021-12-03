347b28f3f4dd60504335345f64144d561020477e
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-03 17:42:34 +0700

    environment variable

--- pages/novel.tsx
+++ pages/novel.tsx
@@ -1,12 +1,24 @@
 import type { GetStaticProps, NextPage } from 'next';
 
 const NovelList: NextPage<{ novels: string }> = ({ novels }) => (
-  <h1 className="content">
-    {novels}
-  </h1>
+  <>
+    <h1 className="content">
+      {novels}
+    </h1>
+    <h2>
+      Env user: {process.env.DB_USER}
+      password: {process.env.DB_PASS}
+      analytics: {process.env.NEXT_PUBLIC_ANALYTICS}
+    </h2>
+  </>
 );
 
 export const getStaticProps: GetStaticProps = async (context) => {
+  const user = process.env.DB_USER;
+  const pass = process.env.DB_PASS;
+
+  console.log(user, pass);
+
   console.log('Running getStaticProps', context.previewData);
 
   return {
