042c7109ce5db47f25f45de086d0fc2ab46df2c4
Hari Surrisyad <hari.surrisyad@gmail.com>
2021-12-03 17:26:48 +0700

    implement redirect from next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      source: '/about',
+      destination: '/',
+      permanent: false,
+    },
+    {
+      source: '/old-blog/:id',
+      destination: '/new-blog/:id',
+      permanent: true,
+    },
+  ],
   devIndicators: {
     buildActivity: false,
   },
